0277ffe4410d84374db97de3926f5236c51ad017
Yash Pandey <pandeyyash002@gmail.com>
2025-02-25 18:26:43 +0530

    added logic

--- src/components/BankStructure.tsx
+++ src/components/BankStructure.tsx
@@ -1,69 +1,111 @@
 import { FC, useEffect, useState } from "react";
 import axios from "axios";
 
-type Bank = {
+interface BankDataResponse<T> {
+    code: number;
+    message: string;
+    success: boolean;
+    data: T;
+}
+
+interface Bank {
     bank_name: string;
     location: string;
-    child_banks: Array<Bank>;
-};
+    valuation: string;
+    date?: string;
+    child_banks?: Bank[];
+}
 
-type BankModalProps = {
-    bankName?: string;
-    location?: string;
-    childBanks?: Bank[];
-};
+type BankModalProps = Bank;
 
-const BankModel: FC<BankModalProps> = ({ bankName, location, childBanks }) => {
+// type BankModalProps = Bank & {
+//     isChecked: boolean;
+// };
+
+const BankModel: FC<BankModalProps> = ({
+    bank_name,
+    location,
+    valuation,
+    date,
+    // isChecked,
+}) => {
     return (
-        <div>
-            <input type="checkbox" />
-            <div>{bankName}</div>
-            <div>{location}</div>
+        <div className="bg-white w-[500px] rounded-xl">
+            <div className="flex gap-2">
+                {/* <input type="checkbox" checked={isChecked} /> */}
+                <input type="checkbox" />
+                <div>
+                    <div>
+                        <div className="font-bold">{bank_name}</div>
+                        <div>({location})</div>
+                    </div>
+                    <div className="flex gap-4">
+                        <span>{date}</span>
+                        <span>{valuation}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-            <div>
-                {childBanks?.map((bank, index) => (
-                    <BankModel
-                        bankName={bank.bank_name}
+const BankModelRenderer: FC<BankModalProps> = ({
+    bank_name,
+    location,
+    valuation,
+    date,
+    child_banks,
+}) => {
+    // const [checkedBanks, setCheckedBanks] = useState();
+    return (
+        <div className="flex flex-col gap-2">
+            <BankModel
+                bank_name={bank_name}
+                location={location}
+                valuation={valuation}
+                date={date ? date : undefined}
+                // isChecked={}
+            />
+            {child_banks?.map((bank: Bank, index) => (
+                <div className="ml-7">
+                    <BankModelRenderer
+                        bank_name={bank.bank_name}
                         location={bank.location}
-                        childBanks={bank.child_banks}
+                        valuation={bank.valuation}
+                        date={bank.date ? bank.date : undefined}
+                        child_banks={bank?.child_banks}
                         key={index}
                     />
-                ))}
-            </div>
+                </div>
+            ))}
         </div>
     );
 };
 
 const BankStructure = () => {
     const [bankData, setBankData] = useState<Bank>();
-    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         async function getData() {
-            const { data } = await axios.get("http://localhost:3000/bank-data");
+            const response = await axios.get("http://localhost:3000/bank-data");
+            const data: BankDataResponse<Bank> = response.data;
             setBankData(data.data);
         }
         getData();
     }, []);
 
-    useEffect(() => {
-        setIsLoading(false);
-    }, [bankData]);
-
-    {
-        if (isLoading || !bankData) return <div>Loading...</div>;
+    if (!bankData) {
+        return <div>Loading...</div>;
     }
 
     return (
-        <div>
-            <div>
-                <BankModel
-                    bankName={bankData?.bank_name}
-                    location={bankData?.location}
-                    childBanks={bankData.child_banks}
-                />
-            </div>
-        </div>
+        <BankModelRenderer
+            bank_name={bankData.bank_name}
+            location={bankData.location}
+            valuation={bankData.valuation}
+            date={bankData.date ? bankData.date : undefined}
+            child_banks={bankData?.child_banks}
+        />
     );
 };
 
